f03a1e37aa4cafaf29c59b1a1957c021e95fb033
Konstantin Voykov <koykov0@gmail.com>
2025-02-05 00:30:28 +0300

    concurrent implementation: cover with simple test/bench

---
+++ concurrent_vector_test.go
@@ -0,0 +1,62 @@
+package bitvector
+
+import "testing"
+
+func TestConcurrentVector(t *testing.T) {
+	prepare := func(size uint) *ConcurrentVector {
+		vec, _ := NewConcurrentVector(10, 1)
+		vec.Set(3)
+		vec.Set(5)
+		vec.Set(7)
+		vec.Set(9)
+		return vec
+	}
+	t.Run("set", func(t *testing.T) {
+		vec := prepare(10)
+		if vec.buf[0] != 680 {
+			t.Fail()
+		}
+	})
+	t.Run("get", func(t *testing.T) {
+		vec := prepare(10)
+		chk := map[int]uint8{3: 1, 5: 1, 7: 1, 9: 1}
+		for i := 0; i < 10; i++ {
+			if chk[i] != vec.Get(uint64(i)) {
+				t.Fail()
+			}
+		}
+	})
+	t.Run("clear", func(t *testing.T) {
+		vec := prepare(10)
+		vec.Clear(5)
+		if vec.Get(5) != 0 {
+			t.Fail()
+		}
+	})
+}
+
+func BenchmarkConcurrentVector(b *testing.B) {
+	b.Run("set", func(b *testing.B) {
+		b.ReportAllocs()
+		vec, _ := NewConcurrentVector(10, 1)
+		for i := 0; i < b.N; i++ {
+			vec.Set(9)
+		}
+	})
+	b.Run("get", func(b *testing.B) {
+		b.ReportAllocs()
+		vec, _ := NewConcurrentVector(10, 1)
+		vec.Set(5)
+		for i := 0; i < b.N; i++ {
+			vec.Get(5)
+		}
+	})
+	b.Run("clear", func(b *testing.B) {
+		b.ReportAllocs()
+		vec, _ := NewConcurrentVector(10, 1)
+		vec.Set(5)
+		for i := 0; i < b.N; i++ {
+			vec.Clear(5)
+		}
+	})
+}
