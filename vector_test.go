4d9f831643dd0defe4399da826ddb507448e5719
Konstantin Voykov <koykov0@gmail.com>
2025-02-10 01:27:16 +0300

    vector: cover reader/writer with test

--- vector_test.go
+++ vector_test.go
@@ -1,6 +1,9 @@
 package bitvector
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestVector(t *testing.T) {
 	prepare := func(size uint) *Vector {
@@ -31,6 +34,36 @@ func TestVector(t *testing.T) {
 		vec.Unset(5)
 		if vec.Get(5) != 0 {
 			t.Fail()
+		}
+	})
+	t.Run("writer", func(t *testing.T) {
+		vec := prepare(10)
+		f, err := os.OpenFile("testdata/vector.bin", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return
+		}
+		n, err := vec.WriteTo(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != 2 {
+			t.Fail()
+		}
+	})
+	t.Run("reader", func(t *testing.T) {
+		vec, _ := NewVector(10)
+		f, err := os.Open("testdata/vector.bin")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err = vec.ReadFrom(f); err != nil {
+			t.Fatal(err)
+		}
+		chk := map[int]uint8{3: 1, 5: 1, 7: 1, 9: 1}
+		for i := 0; i < 10; i++ {
+			if chk[i] != vec.Get(uint64(i)) {
+				t.Fail()
+			}
 		}
 	})
 }
